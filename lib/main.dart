ffd64a2277f8c677f8b8cf4fb9bbff60a105e677
Anhad <84476084+07anhad@users.noreply.github.com>
2022-05-03 18:07:19 +0530

    routes basics done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:fluttercatalog/pages/home_page.dart';
+import 'package:fluttercatalog/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,13 +13,21 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     
     return MaterialApp(
-      home:HomePage(),
-      themeMode: ThemeMode.dark,
+        themeMode: ThemeMode.light,
       theme: ThemeData(primarySwatch: Colors.deepPurple),
       darkTheme: ThemeData(
         primarySwatch: Colors.red,
         brightness: Brightness.dark,
       ),
+
+        initialRoute: "/home",
+        routes: {
+          // "/": (context) => HomePage(),
+          "/": (context) => LoginPage(),
+          "/home": (context) => HomePage(),
+          "/login": (context) => LoginPage(),
+        }
+
     );
   }
 }
