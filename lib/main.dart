2177a612267cc0855b7ff6872103a7fa67127270
Anhad <84476084+07anhad@users.noreply.github.com>
2022-05-04 17:37:41 +0530

    singlescrollview and navigator

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:fluttercatalog/pages/home_page.dart';
 import 'package:fluttercatalog/pages/login_page.dart';
+import 'package:fluttercatalog/utils/routes.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
@@ -19,17 +20,18 @@ class MyApp extends StatelessWidget {
           primarySwatch: Colors.deepPurple,
           fontFamily: GoogleFonts.baloo().fontFamily,
         ),
-      darkTheme: ThemeData(
-        primarySwatch: Colors.red,
-        brightness: Brightness.dark,
-      ),
+        // debugShowCheckedModeBanner: false,
+        darkTheme: ThemeData(
+          primarySwatch: Colors.red,
+          brightness: Brightness.dark,
+        ),
 
         initialRoute: "/login",
         routes: {
           // "/": (context) => HomePage(),
           "/": (context) => LoginPage(),
-          "/home": (context) => HomePage(),
-          // "/login": (context) => LoginPage(),
+          MyRoutes.homeRoute: (context) => HomePage(),
+          MyRoutes.loginRoute: (context) => LoginPage()
         }
 
     );
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluttercatalog/utils/routes.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -7,53 +8,60 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Material(
         color: Colors.white,
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 20.0,
-            ),
-            Image.asset(
-              "assets/images/login-image.png",
-              fit: BoxFit.cover,
-            ),
-            const SizedBox(
-              height: 20.0,
-            ),
-            const Text(
-              "Welcome",
-              style: TextStyle(fontSize: 26, fontWeight: FontWeight.w300),
-            ),
-            const SizedBox(
-              height: 20.0,
-            ),
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 28.0),
-              child: Column(
-                children: [
-                  TextFormField(
-                    decoration: const InputDecoration(
-                        hintText: "Enter username", labelText: "username"),
-                  ),
-                  TextFormField(
-                    obscureText: true,
-                    decoration: const InputDecoration(
-                        hintText: "Enter password", labelText: "password"),
-                  ),
-                  const SizedBox(
-                    height: 20.0,
-                  ),
-                  ElevatedButton(
-                    child: const Text("Login"),
-                    onPressed: () {
-                      // ignore: avoid_print
-                      print("helloo anhad");
-                    },
-                  )
-                ],
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 20.0,
               ),
-            )
-          ],
+              Image.asset(
+                "assets/images/login-image.png",
+                fit: BoxFit.cover,
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              const Text(
+                "Welcome",
+                style: TextStyle(fontSize: 26, fontWeight: FontWeight.w300),
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                    vertical: 16.0, horizontal: 28.0),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: const InputDecoration(
+                          hintText: "Enter username", labelText: "username"),
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: const InputDecoration(
+                          hintText: "Enter password", labelText: "password"),
+                    ),
+                    const SizedBox(
+                      height: 40.0,
+                    ),
+                    ElevatedButton(
+                      child: const Text("Login",
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.w100)),
+                      style: TextButton.styleFrom(
+                          minimumSize: const Size(150, 40)),
+                      onPressed: () {
+                        // ignore: avoid_print
+                        // print("helloo anhad");
+                        Navigator.pushNamed(context, MyRoutes.homeRoute);
+                      },
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
         ));
   }
 }
