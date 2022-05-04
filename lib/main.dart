cf62dc31179aec7b238402745c71d9d4af91fdb5
Anhad <84476084+07anhad@users.noreply.github.com>
2022-05-04 16:50:10 +0530

    removed the blue squigly lines and used textformfield

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:fluttercatalog/pages/home_page.dart';
 import 'package:fluttercatalog/pages/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,18 +15,21 @@ class MyApp extends StatelessWidget {
     
     return MaterialApp(
         themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
+        theme: ThemeData(
+          primarySwatch: Colors.deepPurple,
+          fontFamily: GoogleFonts.baloo().fontFamily,
+        ),
       darkTheme: ThemeData(
         primarySwatch: Colors.red,
         brightness: Brightness.dark,
       ),
 
-        initialRoute: "/home",
+        initialRoute: "/login",
         routes: {
           // "/": (context) => HomePage(),
           "/": (context) => LoginPage(),
           "/home": (context) => HomePage(),
-          "/login": (context) => LoginPage(),
+          // "/login": (context) => LoginPage(),
         }
 
     );
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -6,16 +6,54 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-        child: Center(
-      child: Text(
-        "Login Page",
-        style: TextStyle(
-          fontSize: 20,
-          color: Colors.blue,
-          fontWeight: FontWeight.bold,
-        ),
-        // textScaleFactor: 4.0,
-      ),
-    ));
+        color: Colors.white,
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 20.0,
+            ),
+            Image.asset(
+              "assets/images/login-image.png",
+              fit: BoxFit.cover,
+            ),
+            const SizedBox(
+              height: 20.0,
+            ),
+            const Text(
+              "Welcome",
+              style: TextStyle(fontSize: 26, fontWeight: FontWeight.w300),
+            ),
+            const SizedBox(
+              height: 20.0,
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 28.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: const InputDecoration(
+                        hintText: "Enter username", labelText: "username"),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                        hintText: "Enter password", labelText: "password"),
+                  ),
+                  const SizedBox(
+                    height: 20.0,
+                  ),
+                  ElevatedButton(
+                    child: const Text("Login"),
+                    onPressed: () {
+                      // ignore: avoid_print
+                      print("helloo anhad");
+                    },
+                  )
+                ],
+              ),
+            )
+          ],
+        ));
   }
 }
