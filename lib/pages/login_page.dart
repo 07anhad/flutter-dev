4ef45859d92cc81fd2a25126f17b10b855cdd2df
Anhad <84476084+07anhad@users.noreply.github.com>
2022-05-05 12:59:55 +0530

    stateful, animated login container and future delay

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:fluttercatalog/utils/routes.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name = "";
+  bool changeButton = false;
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +20,7 @@ class LoginPage extends StatelessWidget {
           child: Column(
             children: [
               const SizedBox(
-                height: 20.0,
+                height: 40.0,
               ),
               Image.asset(
                 "assets/images/login-image.png",
@@ -21,8 +29,8 @@ class LoginPage extends StatelessWidget {
               const SizedBox(
                 height: 20.0,
               ),
-              const Text(
-                "Welcome",
+              Text(
+                "Welcome $name",
                 style: TextStyle(fontSize: 26, fontWeight: FontWeight.w300),
               ),
               const SizedBox(
@@ -36,6 +44,11 @@ class LoginPage extends StatelessWidget {
                     TextFormField(
                       decoration: const InputDecoration(
                           hintText: "Enter username", labelText: "username"),
+                      onChanged: (value) {
+                        name = value;
+                        setState(
+                            () {}); //stateful widget add dynamic changes to your screen
+                      },
                     ),
                     TextFormField(
                       obscureText: true,
@@ -45,18 +58,49 @@ class LoginPage extends StatelessWidget {
                     const SizedBox(
                       height: 40.0,
                     ),
-                    ElevatedButton(
-                      child: const Text("Login",
-                          style: TextStyle(
-                              fontSize: 18, fontWeight: FontWeight.w100)),
-                      style: TextButton.styleFrom(
-                          minimumSize: const Size(150, 40)),
-                      onPressed: () {
-                        // ignore: avoid_print
-                        // print("helloo anhad");
+                    InkWell(
+                      onTap: () async {
+                        setState(() {
+                          changeButton = true;
+                        });
+
+                        await Future.delayed(Duration(seconds: 1));
                         Navigator.pushNamed(context, MyRoutes.homeRoute);
                       },
+                      child: AnimatedContainer(
+                        duration: Duration(seconds: 1),
+                        height: 50,
+                        width: changeButton ? 50 : 130,
+                        alignment: Alignment.center,
+                        child: changeButton
+                            ? Icon(
+                                Icons.done,
+                                color: Colors.white,
+                                size: 20,
+                              )
+                            : Text(
+                                "Login",
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 18),
+                              ),
+                        decoration: BoxDecoration(
+                            color: Colors.deepPurple,
+                            borderRadius:
+                                BorderRadius.circular(changeButton ? 40 : 8)),
+                      ),
                     )
+                    // ElevatedButton(
+                    //   child: const Text("Login",
+                    //       style: TextStyle(
+                    //           fontSize: 18, fontWeight: FontWeight.w100)),
+                    //   style: TextButton.styleFrom(
+                    //       minimumSize: const Size(150, 40)),
+                    //   onPressed: () {
+                    //     // ignore: avoid_print
+                    //     // print("helloo anhad");
+                    //     Navigator.pushNamed(context, MyRoutes.homeRoute);
+                    //   },
+                    // )
                   ],
                 ),
               )
